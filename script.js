17995923950bf0a9ac1ba6044bdf7d97aaee18cc
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-09-17 13:19:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,70 +1,16 @@
-class MinHeap {
-    constructor() {
-        this.heap = [];
-    }
+function minCost(arr) {
+  if (arr.length < 2) {
+    return -Infinity;
+  }
 
-    insert(value) {
-        this.heap.push(value);
-        this.heapifyUp();
-    }
+  let totalCost = 0;
 
-    extractMin() {
-        if (this.isEmpty()) {
-            return null;
-        }
+  while (arr.length > 1) {
+    arr.sort((a, b) => a - b);
+    const cost = arr[0] + arr[1];
+    totalCost += cost;
+    arr.splice(0, 2, cost);
+  }
 
-        if (this.heap.length === 1) {
-            return this.heap.pop();
-        }
-
-        const minValue = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this.heapifyDown();
-        return minValue;
-    }
-
-    size() {
-        return this.heap.length;
-    }
-
-    isEmpty() {
-        return this.size() === 0;
-    }
-
-    heapifyUp() {
-        let currentIndex = this.size() - 1;
-        while (currentIndex > 0) {
-            const parentIndex = Math.floor((currentIndex - 1) / 2);
-            if (this.heap[currentIndex] < this.heap[parentIndex]) {
-                [this.heap[currentIndex], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[currentIndex]];
-                currentIndex = parentIndex;
-            } else {
-                break;
-            }
-        }
-    }
-
-    heapifyDown() {
-        let currentIndex = 0;
-        while (true) {
-            const leftChildIndex = 2 * currentIndex + 1;
-            const rightChildIndex = 2 * currentIndex + 2;
-            let smallestChildIndex = currentIndex;
-
-            if (leftChildIndex < this.size() && this.heap[leftChildIndex] < this.heap[smallestChildIndex]) {
-                smallestChildIndex = leftChildIndex;
-            }
-
-            if (rightChildIndex < this.size() && this.heap[rightChildIndex] < this.heap[smallestChildIndex]) {
-                smallestChildIndex = rightChildIndex;
-            }
-
-            if (smallestChildIndex !== currentIndex) {
-                [this.heap[currentIndex], this.heap[smallestChildIndex]] = [this.heap[smallestChildIndex], this.heap[currentIndex]];
-                currentIndex = smallestChildIndex;
-            } else {
-                break;
-            }
-        }
-    }
+  return totalCost;
 }
