b25e47700fcc3fb2d874981fd2e90de029a4d2b5
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-09-18 11:58:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-function minCost(arr) {
-    let n = arr.length;
-    let minHeap = new MinHeap();
-    for (let i = 0; i < n; i++) {
-        minHeap.insert(arr[i]);
-    }
-    let cost = 0;
-    while (minHeap.size() > 1) {
-        let firstMin = minHeap.extractMin();
-        let secondMin = minHeap.extractMin();
-        let sum = firstMin + secondMin;
-        cost += sum;
-        minHeap.insert(sum);
-    }
-    return cost;
-}
+
