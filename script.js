11ed687e03e654cd205b36bb36c7407f47e75e9a
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-09-18 11:42:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-function minCost(arr) {
-  if (arr.length < 2) {
-    return -Infinity;
-  }
 
-  let totalCost = 0;
-
-  while (arr.length > 1) {
-    arr.sort((a, b) => a - b);
-    const cost = arr[0] + arr[1];
-    totalCost += cost;
-    arr.splice(0, 2, cost);
-  }
-
-  return totalCost;
-}
